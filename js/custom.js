ef42da4b6cfb1210ad4fb300fcf67279b2642b44
Xingyu Lin <abclin6693@gmail.com>
2022-08-08 18:09:12 -0400

    show selected publications

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,23 @@ function copy(dest, source) {
     }
     dest.blur();
 }
+
+function showPubs(id) {
+    console.log(id);
+    if (id == 0) {
+        document.getElementById('pubs').innerHTML = document.getElementById('pubs_selected').innerHTML;
+        document.getElementById('select0').style = 'text-decoration:underline;color:#000000';
+        document.getElementById('select1').style = '';
+        // document.getElementById('select2').style = '';
+    } else if (id == 1) {
+        document.getElementById('pubs').innerHTML = document.getElementById('pubs_all_by_date').innerHTML;
+        document.getElementById('select1').style = 'text-decoration:underline;color:#000000';
+        document.getElementById('select0').style = '';
+        // document.getElementById('select2').style = '';
+    } else {
+        document.getElementById('pubs').innerHTML = document.getElementById('pubs_by_topic').innerHTML;
+        document.getElementById('select2').style = 'text-decoration:underline;color:#000000';
+        document.getElementById('select0').style = '';
+        // document.getElementById('select1').style = '';
+    }
+}
